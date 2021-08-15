c1780d796007b0f8ae8257ae5e149d3fa58841e2
ferrazfran <franferraz45@gmail.com>
2021-08-15 11:49:04 -0300

    Função de alterção e início do consulta produto

--- src/controledeestoque/ControleDeEstoque.java
+++ src/controledeestoque/ControleDeEstoque.java
@@ -11,15 +11,15 @@ public class ControleDeEstoque {
         Produto produto = new Produto();
         ArrayList<Produto> listProd = new ArrayList();
         Scanner scan = new Scanner(System.in);
+        
+        
         Produto produto1 = new Produto("nome 1",19,"kg",2);
         Produto produto2 = new Produto("nome 2",11,"L",22);
         Estoque.estoqueLista.add(produto1);
         Estoque.estoqueLista.add(produto2);
         System.out.println(Estoque.estoqueLista.indexOf(produto2));
         System.out.println(Estoque.estoqueLista.stream().filter(customer -> "nome 2".equals(customer.getNome())).findAny().orElse(null) == produto2);
-//        Produto produtoAux = Estoque.estoqueLista.stream().filter(customer -> "nome 2".equals(customer.getNome())).findAny().orElse(null);
-//         Estoque.estoqueLista.remove(produtoAux);
-//         System.out.println(Estoque.estoqueLista.size());
+
         Functions functions = new Functions();
         functions.showMenu(produto, listProd, scan);
         
--- src/controledeestoque/Functions.java
+++ src/controledeestoque/Functions.java
@@ -66,11 +66,16 @@ public class Functions {
                         opcaoSubmenu = 0;
                         break;
                     }
-                    showMenuUpdate(produto, listProd, scan, opcaoSubmenu);
+                    opcaoSubmenu = showMenuUpdate(produto, scan);
+                    break;
+                case 3:
+//                    opcaoSubmenu = showMenuConsultaProduto();
                     break;
                 case 4:
                     showMenuExclusion(produto, scan);
                     break;
+                case 6:
+                    continue;
                 default:
                     break;
             }
@@ -98,7 +103,7 @@ public class Functions {
                 System.out.println("Preço:");
                 scan = new Scanner(System.in);
                 produto.setPreco(scan.nextFloat());
-            }else{
+            } else {
                 produto.setPreco(valor);
             }
 
@@ -119,7 +124,7 @@ public class Functions {
                 System.out.println("Preço:");
                 scan = new Scanner(System.in);
                 produto.setQuantidade(scan.nextInt());
-            }else{
+            } else {
                 produto.setQuantidade(qtd);
             }
         } while (qtd < 0);
@@ -129,9 +134,6 @@ public class Functions {
         if (confirmacao.equals("s") || confirmacao.equals("S")) {
             //TODO salva no array
             Estoque.estoqueLista.add(produto);
-            System.out.println("Estoque.estoqueLista.size()");
-            System.out.println(Estoque.estoqueLista.size());
-//            listProd.add(produto);
         }
         produto = new Produto();
         System.out.println("REPETIR OPERAÇÃO (S/N)? _");
@@ -147,64 +149,80 @@ public class Functions {
         //TODO Deve-se pedir a confirmação da inclusão antes de efetuá-la, caso não seja confirmada, deve-se ignorar o cadastro.
     }
 
-    private void showMenuUpdate(Produto produto, ArrayList<Produto> listProd, Scanner scan, int opcaoSubmenu) {
+    private int showMenuUpdate(Produto produto, Scanner scan) {
         System.out.println("EMPRESA DE IMPORTAÇÃO DE PRODUTOS LTDA.");
         System.out.println("SISTEMA DE CONTROLE DE ESTOQUE:");
         System.out.println("ALTERAÇÃO DE PRODUTO:");
-        System.out.println("1 - INCLUSÃO; 2 - ALTERAÇÃO; 3 - CONSULTA; 4 - EXCLUSÃO; 0 - RETORNAR");
-        System.out.println("OPÇÃO:_:");
-        scan = new Scanner(System.in);
 
         System.out.println("NOME:");//TODO Deve-se validar o nome do produto, não podem existir produtos de mesmo nome.
-        String name = scan.next();
-        System.out.println(name);
-        produto.setNome(name);
-
-        System.out.println("PREÇO:");
         scan = new Scanner(System.in);
-        float valor = scan.nextFloat();
-        System.out.println(valor);
-        do {
-            if (valor <= 0) {
-                System.out.println("O preço deve ser maior que zero.");
-                System.out.println("Preço:");
-                scan = new Scanner(System.in);
-                produto.setPreco(scan.nextFloat());
-            }
+        String nameToVerify = scan.nextLine();
+        Produto produtoAuxToUpdate = Estoque.estoqueLista.stream().filter((product) -> nameToVerify.equals(product.getNome())).findAny().orElse(null);
+        //consultaProduto(produto.getnome())
+        if (produtoAuxToUpdate != null) {
+            System.out.println("Nome: " + produtoAuxToUpdate.getNome());
+            System.out.println("Preço: " + produtoAuxToUpdate.getPreco());
+            System.out.println("Unidade: " + produtoAuxToUpdate.getUnidade());
+            System.out.println("Quantidade: " + produtoAuxToUpdate.getQuantidade());
 
-        } while (valor <= 0);
+            System.out.println("NOME:");//TODO Deve-se validar o nome do produto, não podem existir produtos de mesmo nome.
+            scan = new Scanner(System.in);
+            String name = scan.nextLine();
+            System.out.println(name);
+            produto.setNome(name);
 
-        System.out.println("UNIDADE:");
-        String unidade = scan.next();
-        System.out.println(unidade);
-        produto.setUnidade(unidade);
+            System.out.println("PREÇO:");
+            scan = new Scanner(System.in);
+            float valor = scan.nextFloat();
+            System.out.println(valor);
+            do {
+                if (valor <= 0) {
+                    System.out.println("O preço deve ser maior que zero.");
+                    System.out.println("Preço:");
+                    scan = new Scanner(System.in);
+                    produto.setPreco(scan.nextFloat());
+                }
 
-        System.out.println("QUANTIDADE:");
-        scan = new Scanner(System.in);
-        int qtd = scan.nextInt();
-        System.out.println(qtd);
-        do {
-            if (qtd < 0) {
-                System.out.println("A quantidade deve ser maior ou igual a zero.");
-                System.out.println("Preço:");
-                scan = new Scanner(System.in);
-                produto.setQuantidade(scan.nextInt());
-            }
-        } while (qtd < 0);
+            } while (valor <= 0);
+
+            System.out.println("UNIDADE:");
+            String unidade = scan.next();
+            System.out.println(unidade);
+            produto.setUnidade(unidade);
+
+            System.out.println("QUANTIDADE:");
+            scan = new Scanner(System.in);
+            int qtd = scan.nextInt();
+            System.out.println(qtd);
+            do {
+                if (qtd < 0) {
+                    System.out.println("A quantidade deve ser maior ou igual a zero.");
+                    System.out.println("Preço:");
+                    scan = new Scanner(System.in);
+                    produto.setQuantidade(scan.nextInt());
+                }
+            } while (qtd < 0);
+        } else {
+            System.out.println("Produto não existe");
+        }
 
         System.out.println("CONFIRMA INCLUSÃO (S/N)? _");
         String confirmacao = scan.next();
-        if (confirmacao == "s" || confirmacao == "S") {
+        if (confirmacao.equals("s") || confirmacao.equals("S")) {
             //TODO salva no array
-            listProd.add(produto);
+            System.out.println(produtoAuxToUpdate);
+            System.out.println(produto);
+            Estoque.estoqueLista.remove(produtoAuxToUpdate);
+            Estoque.estoqueLista.add(produto);
         }
         produto = new Produto();
         System.out.println("REPETIR OPERAÇÃO (S/N)? _");
         String confirmacaoRepetir = scan.next();
-        if (confirmacaoRepetir != "s" || confirmacaoRepetir != "S") {
+        if (!confirmacaoRepetir.equals("s") || !confirmacaoRepetir.equals("S")) {
             //TODO salva no array
-            opcaoSubmenu = 0;
+            return 0;
         }
+        return 6;
 
         //TODO Deve-se pedir a confirmação da inclusão antes de efetuá-la, caso não seja confirmada, deve-se ignorar o cadastro.
     }
@@ -212,36 +230,36 @@ public class Functions {
     private void showProductsLists(ArrayList<Produto> listProd) {
         System.out.println(Estoque.estoqueLista.size());
         for (Produto produto : Estoque.estoqueLista) {
-            System.out.println("Nome: "+produto.getNome());
-            System.out.println("Preço: "+produto.getPreco());
-            System.out.println("Unidade: "+produto.getUnidade());
-            System.out.println("Quantidade: "+produto.getQuantidade());
+            System.out.println("---------------------------------");
+            System.out.println("Nome: " + produto.getNome());
+            System.out.println("Preço: " + produto.getPreco());
+            System.out.println("Unidade: " + produto.getUnidade());
+            System.out.println("Quantidade: " + produto.getQuantidade());
+            System.out.println("---------------------------------");
         }
     }
 
-    private int showMenuExclusion(Produto produto, Scanner scan){
+    private int showMenuExclusion(Produto produto, Scanner scan) {
         System.out.println("EMPRESA DE IMPORTAÇÃO DE PRODUTOS LTDA.");
         System.out.println("SISTEMA DE CONTROLE DE ESTOQUE:");
         System.out.println("EXCLUSÃO DE PRODUTO:");
         System.out.println("NOME:");//TODO Deve-se validar o nome do produto, não podem existir produtos de mesmo nome.
         scan = new Scanner(System.in);
         String name = scan.nextLine();
-        System.out.println(name);
-        Produto produtoAux = Estoque.estoqueLista.stream().filter(customer -> name.equals(customer.getNome())).findAny().orElse(null);
-        System.out.println("Estoque.estoqueLista.size():");
-        System.out.println(Estoque.estoqueLista.size());
-        System.out.println("produtoAux:");
-        System.out.println(produtoAux);
-        if(produtoAux != null){
+        Produto produtoAux = Estoque.estoqueLista.stream().filter(product -> name.equals(product.getNome())).findAny().orElse(null);
+        if (produtoAux != null) {
             Estoque.estoqueLista.remove(produtoAux);
-            System.out.println(Estoque.estoqueLista.size());
-        }else{
+        } else {
             System.out.println("Produto não existe");
         }
-        
+
         return 0;
-        
-//        Estoque.estoqueLista.get(0)
+
     }
     
+    private Produto consultaProduto(String nome){
+        Produto produtoAuxToUpdate = Estoque.estoqueLista.stream().filter((product) -> nome.equals(product.getNome())).findAny().orElse(null);
+        return produtoAuxToUpdate;
+    }
+
 }
